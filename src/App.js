0b66a2b06605a5be503c9ba4a897e54980aee30c
Janek Stoeck <janek.stoeck@akra.de>
2020-12-10 13:17:25 +0100

    resolved the issue

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>I have been changed!</p>
         <p>I was changed on the feature branch!</p>
       </header>
     </div>
